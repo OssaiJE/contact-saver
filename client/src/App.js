4fc969cd60ae2d9f3797f8cf116e2e2415f6c856
OssaiJE <sirjuliusdev@gmail.com>
2020-12-11 04:33:28 +0100

    Started working on the authentication with backend

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,29 @@ import About from "./components/pages/About";
 import Home from "./components/pages/Home";
 import NotFound from "./components/pages/NotFound";
 import Navbar from "./components/layout/Navbar";
+
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
 import "./App.css";
 
 const App = () => {
 	return (
-		<ContactState>
-			<Router>
-				<Fragment>
-					<Navbar />
-					<div className="container">
-						<Switch>
-							<Route exact path="/" component={Home} />
-							<Route exact path="/about" component={About} />
-							<Route component={NotFound} />
-						</Switch>
-					</div>
-				</Fragment>
-			</Router>
-		</ContactState>
+		<AuthState>
+			<ContactState>
+				<Router>
+					<Fragment>
+						<Navbar />
+						<div className="container">
+							<Switch>
+								<Route exact path="/" component={Home} />
+								<Route exact path="/about" component={About} />
+								<Route component={NotFound} />
+							</Switch>
+						</div>
+					</Fragment>
+				</Router>
+			</ContactState>
+		</AuthState>
 	);
 };
 

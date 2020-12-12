70ca33bdd85adcc6461186ae85930fe074bb3b4d
OssaiJE <sirjuliusdev@gmail.com>
2020-12-12 01:46:34 +0100

    Completed the user reg with backend api

---
+++ client/src/context/auth/authReducer.js
@@ -0,0 +1,68 @@
+import {
+	REGISTER_SUCCESS,
+	REGISTER_FAIL,
+	USER_LOADED,
+	AUTH_ERROR,
+	LOGIN_SUCCESS,
+	LOGIN_FAIL,
+	LOGOUT,
+	CLEAR_ERRORS,
+} from "../types";
+
+const authReducer = (state, action) => {
+	switch (action.type) {
+		case REGISTER_SUCCESS:
+			localStorage.setItem("token", action.payload.token);
+			return {
+				...state,
+				...action.payload,
+				isAuthenticated: true,
+				loading: false,
+			};
+		case REGISTER_FAIL:
+			localStorage.removeItem("token");
+			return {
+				...state,
+				token: null,
+				isAuthenticated: false,
+				loading: false,
+				user: null,
+				error: action.payload,
+			};
+		// case SET_CURRENT:
+		// 	return {
+		// 		...state,
+		// 		current: action.payload,
+		// 	};
+		// case CLEAR_CURRENT:
+		// 	return {
+		// 		...state,
+		// 		current: null,
+		// 	};
+		// case UPDATE_CONTACT:
+		// 	return {
+		// 		...state,
+		// 		contacts: state.contacts.map((contact) =>
+		// 			contact.id === action.payload.id ? action.payload : contact
+		// 		),
+		// 	};
+		// case FILTER_CONTACTS:
+		// 	return {
+		// 		...state,
+		// 		filtered: state.contacts.filter((contact) => {
+		// 			// the "gi" parameter means global and insensitive
+		// 			const regex = new RegExp(`${action.payload}`, "gi");
+		// 			return contact.name.match(regex) || contact.email.match(regex);
+		// 		}),
+		// 	};
+		case CLEAR_ERRORS:
+			return {
+				...state,
+				error: null,
+			};
+		default:
+			return state;
+	}
+};
+
+export default authReducer;

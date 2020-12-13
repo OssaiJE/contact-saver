9bd536f95624f2b3b5b7050c8aae6431d8f579bb
OssaiJE <sirjuliusdev@gmail.com>
2020-12-13 02:41:45 +0100

    Web app functionality complete...remaining finishing touch

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -63,19 +63,44 @@ const ContactState = (props) => {
 	};
 
 	// DELETE CONTACT
-	const deleteContact = (id) => {
-		dispatch({
-			type: DELETE_CONTACT,
-			payload: id,
-		});
+	const deleteContact = async (id) => {
+		try {
+			await axios.delete(`/api/contacts/${id}`);
+			dispatch({
+				type: DELETE_CONTACT,
+				payload: id,
+			});
+		} catch (err) {
+			dispatch({
+				type: CONTACT_ERROR,
+				payload: err.response.msg,
+			});
+		}
 	};
 
 	// UPDATE CONTACT
-	const updateContact = (contact) => {
-		dispatch({
-			type: UPDATE_CONTACT,
-			payload: contact,
-		});
+	const updateContact = async (contact) => {
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
+		try {
+			const res = await axios.put(
+				`/api/contacts/${contact._id}`,
+				contact,
+				config
+			);
+			dispatch({
+				type: UPDATE_CONTACT,
+				payload: res.data,
+			});
+		} catch (err) {
+			dispatch({
+				type: CONTACT_ERROR,
+				payload: err.response.msg,
+			});
+		}
 	};
 
 	// READ | SET CURRENT CONTACT
